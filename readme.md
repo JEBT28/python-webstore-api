263de00952d4d03da907a94fc4541899bbf41a76
Juan Baltierrez <juanbal2808@gmail.com>
2022-11-15 22:13:11 -0600

    Readme

---
+++ readme.md
@@ -0,0 +1,31 @@
+# Configurar entorno virtual
+
+Comando para crear entorno virtual
+
+```
+python -m venv venv
+```
+
+Comando para activar entorno virtual
+
+```
+venv\Scripts\activate
+```
+
+Comando para desactivar entorno virtual
+
+```
+deactivate
+```
+
+Comando para instalar dependencias
+
+```
+pip install -r requirements.txt
+```
+
+Comando para ejecutar el proyecto
+
+```
+python manage.py runserver
+```
--- webstore-main/urls.py
+++ webstore-main/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
+ 
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from . import views
 urlpatterns = [
+    path("", views.index, name="index"),
     path('admin/', admin.site.urls),
-    path('auth/', include('auth.urls')),
+    path('autenticacion/', include('autenticacion.urls')),
     path('productos/', include('productos.urls')),
     path('tienda/', include('tienda.urls')),
 ]
