fc7960710fffaa8a92f8bad8ecc550f2f6d04b47
Juan Baltierrez <juanbal2808@gmail.com>
2022-11-15 20:41:35 -0600

    apps

--- webstore-main/settings.py
+++ webstore-main/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth',
+    'productos',
+    'tienda',
+    'usuarios'
 ]
 
 MIDDLEWARE = [
